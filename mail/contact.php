1b032474f70e8ce558267bb7c262e35e8135843e
Bastiaan <b.vankleef@student.han.nl>
2022-06-22 20:29:33 +0200

    PHP test

--- mail/contact.php
+++ mail/contact.php
@@ -1,4 +1,6 @@
 <?php
+include $_SERVER['DOCUMENT_ROOT']."/PHP/Capatcha.php";
+
 if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
@@ -9,7 +11,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "bastiaan228@live.nl";
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
